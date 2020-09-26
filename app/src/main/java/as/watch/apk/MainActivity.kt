a9e663701e12962e2f76bfa1d9aa8b6dfd86df5b
Akinzou <wiktorjn@gmail.com>
2020-09-26 18:57:36 +0200

    Initial commit

---
+++ app/src/main/java/as/watch/apk/MainActivity.kt
@@ -0,0 +1,11 @@
+package `as`.watch.apk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
